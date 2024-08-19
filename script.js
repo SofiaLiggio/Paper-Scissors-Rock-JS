ed294444fdcbc97efe3b20a54cf25521312f5f13
Sofia Liggio <sofialiggio994@gmail.com>
2024-08-19 22:50:42 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,23 @@
+const playerChoose = prompt("PAPER - SCISSORS - ROCK");
+const choose = ["paper", "scissors", "rock"];
+const randomMove = Math.floor(Math.random() * choose.length);
+const adversary = choose[randomMove];
+console.log(randomMove);
+
+let result;
+console.log(playerChoose);
+console.log(adversary);
+
+if (playerChoose === adversary) {
+  result = "DRAW!";
+} else if (
+  (playerChoose === "PAPER" && adversary === "ROCK") ||
+  (playerChoose === "SCISSORS" && adversary === "PAPER") ||
+  (playerChoose === "ROCK" && adversary === "PAPER")
+) {
+  result = "WIN!";
+} else {
+  result = "LOSER!";
+}
+
+console.log(result);
