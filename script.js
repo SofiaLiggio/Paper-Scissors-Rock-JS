aae4cfbbeb87255ae73bfb63f09a2f814f4f04d9
sofia_liggio <sofialiggio994@gmail.com>
2024-08-20 12:57:06 +0200

    finish project

--- script.js
+++ script.js
@@ -1,23 +1,34 @@
-const playerChoose = prompt("PAPER - SCISSORS - ROCK");
-const choose = ["paper", "scissors", "rock"];
-const randomMove = Math.floor(Math.random() * choose.length);
-const adversary = choose[randomMove];
-console.log(randomMove);
+while (true) {
+  const playerChoose = prompt("PAPER - SCISSORS - ROCK").toUpperCase();
+  const choose = ["PAPER", "SCISSORS", "ROCK"];
+  const randomMove = Math.floor(Math.random() * choose.length);
+  const adversary = choose[randomMove];
 
-let result;
-console.log(playerChoose);
-console.log(adversary);
+  let result;
 
-if (playerChoose === adversary) {
-  result = "DRAW!";
-} else if (
-  (playerChoose === "PAPER" && adversary === "ROCK") ||
-  (playerChoose === "SCISSORS" && adversary === "PAPER") ||
-  (playerChoose === "ROCK" && adversary === "PAPER")
-) {
-  result = "WIN!";
-} else {
-  result = "LOSER!";
+  if (playerChoose === adversary) {
+    result = "DRAW!";
+  } else if (
+    (playerChoose === "PAPER" && adversary === "ROCK") ||
+    (playerChoose === "SCISSORS" && adversary === "PAPER") ||
+    (playerChoose === "ROCK" && adversary === "SCISSORS")
+  ) {
+    result = `${playerChoose} WINS AGAINST ${adversary}!`;
+  } else if (
+    (adversary === "PAPER" && playerChoose === "ROCK") ||
+    (adversary === "SCISSORS" && playerChoose === "PAPER") ||
+    (adversary === "ROCK" && playerChoose === "SCISSORS")
+  ) {
+    result = `${playerChoose} LOSES AGAINST ${adversary}! `;
+  } else {
+    alert("INVALID MOVE! CHOOSE PAPER, SCISSORS OR ROCK.");
+    continue;
+  }
+
+  console.log(result);
+  let playAgain = prompt("DO YOU WANT CONTINUE TO PLAY? Y/N").toUpperCase();
+
+  if (playAgain === "N") {
+    break;
+  }
 }
-
-// console.log(result);
